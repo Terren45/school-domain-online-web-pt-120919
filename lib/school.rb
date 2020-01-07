879f5de204a90a5ca0ac401b5fc7bc4e18fbf04a
Terren Culberson <culbersonterren@yahoo.com>
2020-01-07 00:26:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,13 @@ class School
     roster[level] ||= []
     roster[level] << student
   end 
-  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
   
   def sort 
   nu_hash = {}
